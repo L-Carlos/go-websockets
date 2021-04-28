007bedbbf96931bce20f823f493456876d0be7c4
Luis Carlos <luiscarlosfirminopinheiro@gmail.com>
2021-04-28 14:23:40 -0300

    simple server setup

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Home Page")
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Websocket Endpoint")
 }
